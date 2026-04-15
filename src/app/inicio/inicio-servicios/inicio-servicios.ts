299df62efc4ac3092a160d0821dc50c6eac5865b
Roman <xxromanxxtm@gmail.com>
2026-04-15 15:55:25 -0300

    arreglo servicios

--- src/app/inicio/inicio-servicios/inicio-servicios.ts
+++ src/app/inicio/inicio-servicios/inicio-servicios.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-inicio-servicios',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio-servicios.html',
   styleUrl: './inicio-servicios.scss',
 })
